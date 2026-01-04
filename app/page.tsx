e5aadb4b7c40562eae14853f8f02e6367f266963
blazmaric <39536499+blazmaric@users.noreply.github.com>
2026-01-04 11:03:02 +0100

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-export const dynamic = 'force-dynamic';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  redirect('/login');
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, [router]);
+
   return null;
 }
