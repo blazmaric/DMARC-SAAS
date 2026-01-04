c1fd21d2bbc266625d69549c2383a29a77875706
blazmaric <39536499+blazmaric@users.noreply.github.com>
2026-01-04 12:34:35 +0100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { Providers } from './providers';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'M-Host DMARC Monitoring',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
+      <body className="font-sans antialiased" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
