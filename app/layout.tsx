3929e84b74324220cb40230326739f4048618f90
blazmaric <39536499+blazmaric@users.noreply.github.com>
2026-01-04 11:42:07 +0100

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,6 @@ import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const dynamic = 'force-dynamic';
-export const dynamicParams = true;
-export const revalidate = 0;
-
 export const metadata: Metadata = {
   title: 'M-Host DMARC Monitoring',
   description: 'Professional DMARC monitoring for Slovenia',
